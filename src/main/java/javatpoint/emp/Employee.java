6055a6ab9db5e25af7206f11c13c206e42b4e4a5
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-05 14:48:46 +0600

    Dependency Injection by Constructor Example

---
+++ src/main/java/javatpoint/emp/Employee.java
@@ -0,0 +1,27 @@
+package javatpoint.emp;
+
+public class Employee {
+    private int id;
+    private String name;
+
+    public Employee(int id) {
+        this.id = id;
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Employee(){
+        System.out.println("default constructor");
+    }
+
+    public void show(){
+        System.out.println("id="+id+", name="+name);
+    }
+}
