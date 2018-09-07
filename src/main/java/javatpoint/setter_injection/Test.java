5cce45c2277c5a7efec000d6982d9e6a6c7056e2
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-07 17:28:11 +0600

    Dependency Injection by setter method

---
+++ src/main/java/javatpoint/setter_injection/Test.java
@@ -0,0 +1,12 @@
+package javatpoint.setter_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Employee employee = (Employee) context.getBean("setter");
+        employee.display();
+    }
+}
