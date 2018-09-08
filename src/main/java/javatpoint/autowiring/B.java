d212af5249071f23830d66b958aeefcb3a7a698f
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-08 12:45:07 +0600

    byName autowiring mode

---
+++ src/main/java/javatpoint/autowiring/B.java
@@ -0,0 +1,11 @@
+package javatpoint.autowiring;
+
+public class B {
+    B() {
+        System.out.println("b is created");
+    }
+
+    void print() {
+        System.out.println("hello b");
+    }
+}
