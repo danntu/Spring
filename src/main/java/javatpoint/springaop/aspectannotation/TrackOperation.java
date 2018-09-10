9fd4f3025edd3c8ea80904e72788b9d40df91527
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-10 17:29:09 +0600

    @Before Example

---
+++ src/main/java/javatpoint/springaop/aspectannotation/TrackOperation.java
@@ -0,0 +1,18 @@
+package javatpoint.springaop.aspectannotation;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class TrackOperation {
+    @Pointcut("execution(* Operation.*(..))")
+    public void k() {
+    }
+
+    @Before("k()")
+    public void myadvice(JoinPoint joinPoint) {
+        System.out.println("additional concern");
+    }
+}
