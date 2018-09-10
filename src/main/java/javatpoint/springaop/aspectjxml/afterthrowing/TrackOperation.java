beac6a90c00b6fb8b1d106acff773f110c575067
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-10 23:00:35 +0600

    aop:after-throwing example

---
+++ src/main/java/javatpoint/springaop/aspectjxml/afterthrowing/TrackOperation.java
@@ -0,0 +1,13 @@
+package javatpoint.springaop.aspectjxml.afterthrowing;
+
+import org.aspectj.lang.JoinPoint;
+
+public class TrackOperation {
+    public void myadvice(JoinPoint jp, Throwable error)//it is advice
+    {
+        System.out.println("additional concern");
+        System.out.println("Method Signature: " + jp.getSignature());
+        System.out.println("Exception is: " + error);
+        System.out.println("end of after throwing advice...");
+    }
+}
