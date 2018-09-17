bb98feb15a881834c5943012f2b3c59523abad13
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-17 09:40:34 +0600

    Example of Using variable in SPEL

---
+++ src/main/java/javatpoint/spel/variable/Calculation.java
@@ -0,0 +1,17 @@
+package javatpoint.spel.variable;
+
+public class Calculation {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int cube() {
+        return number * number * number;
+    }
+}
